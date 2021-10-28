3f7e190b0196e9493cc5e4048f93744c77ae7c9b
zoujunqi <55213549+zoujunqi@users.noreply.github.com>
2021-10-29 02:18:38 +0800

    Update views.py
    for specific post view

--- social_network/Post/views.py
+++ social_network/Post/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.views.generic import CreateView
 from Author.models import User, Inbox, Post
-from friends.models import Follower
+from friends.models import Friend
 from rest_framework.response import Response
 import uuid
 from rest_framework import serializers
@@ -31,6 +31,13 @@ def get_author_id(request,auth_id):
     return f"{re_path}/author/{auth_id}"
 
 
+def process_categories(categories):
+    categories = categories.split(',')
+    for i in range(len(categories)):
+        categories[i] = categories[i].strip()
+    return str(categories)
+
+
 class NewPostView(View):
     def get(self, request):
         return render(request, 'new_post.html', None)
@@ -40,72 +47,24 @@ class NewPostView(View):
         title = request.POST.get('title', '')
         content_type = request.POST.get('content_type', '')
         content = request.POST.get('content', '')
-        categories = request.POST.get('categories', '')  # TODO
-        description = request.POST.get('description', '')
-        # source = request.get('source', '')
-        # origin = request.get('origin', '')
-        source = request.build_absolute_uri()  # TODO
-        origin = request.build_absolute_uri()  # TODO
-        #comments = ''  # TODO
 
-        # unlisted = request.get('unlisted', False)
+        categories = request.POST.get('categories', '')  # TODO
+        categories = process_categories(categories)
+
+        description = request.POST.get('description', '')
+        source = request.build_absolute_uri(request.path)  
+        origin = request.build_absolute_uri(request.path)
         unlisted = False   # TODO
         post_id = uuid.uuid4().hex
         post_id = str(post_id)
         visibility = int(request.POST.get('visibility', ''))
-        data1 = {'title': title, 'id': f"{author_id}/posts/{post_id}",
-                'source': source, 'origin': origin, 'description': description,
-                'contentType': content_type, 'content': content, 'author': request.user,
-                'categories': categories,
-                'visibility': visibility, 'unlisted': unlisted}
-        print(data1)
-        # inf_store = PostSerializer(data=data1)
         Post.objects.create(title=title, id=f"{author_id}/posts/{post_id}", source=source, origin=origin, description=description,
                             contentType=content_type, content=content, author=request.user, categories=categories,
                             visibility=visibility, unlisted=unlisted)
-        # if inf_store.is_valid():
-        #     inf_store.save()
         return redirect(reverse('Author:index'))
-        #     # return Response(inf_store.data, 200) TODO:
-        # else:
-        #     return Response(inf_store.data, 400)  # TODO
 
-
-# Creation URL
-class URLPostsView(View):
-    # # POST
-    # def create_post(self, request,*args, **kwargs):
-    #    
-    #     author_id = request.user
-    #     title = request.get('title', '')
-    #     contentType = request.get('contentType', '')
-    #     content = request.get('content', '')
-    #     source = request.get('source', '')
-    #     origin = request.get('origin', '')
-    #     categories = request.get('categories', '')
-    #     size = request.get('size', '')
-    #     count = request.get('count', '')
-    #     comments = request.get('comments', '')
-    #     unlisted = request.get('unlisted', False)
-    #     post_id = uuid.uuid4().hex
-    #     post_id = str(post_id)
-    #     description = request.get('description ', '')
-    #     visibility = request.get('visibility', '')
-    #     data1 = {'title': title, 'id': f"{author_id}/posts/{post_id}",
-    #              'source': source, 'origin': origin, 'description': description,
-    #              'contentType': contentType, 'content': content, 'author': author_id,
-    #              'categories': categories, 'count': count, 'size': size,
-    #              'comments': comments, 'visibility': visibility, 'unlisted': unlisted}
-    #     inf_store = PostSerializer(data=data1)
-    #     inf_store.save()
-    #     return Response(inf_store.data, 200)
-
-    # GET all posts
-    def get_author_posts(self, request,*args, **kwargs):
-        author_id = request.user
-        author_posts = Post.objects.filter(author=author_id)
-        inf_ret = PostSerializer(author_posts, many=True)
-        return Response(inf_ret.data)
+    def select_private(self, request): #TODO
+        return None
 
 
 # with post_id
@@ -117,87 +76,127 @@ class PostsView(View):
         inf_ret = PostSerializer(posts)
         return Response(inf_ret.data)
 
-    # POST
-    def update_post(self, request,*args, **kwargs):
-        post_id = request.get('id', '')
-        posts = Post.objects.get(id=post_id)
-        title_update = request.data.get('title', '')
-        contentType_update = request.data.get('contentType', '')
-        content_update = request.data.get('content', '')
-        source_update = request.data.get('source', '')
-        origin_update = request.data.get('origin', '')
-        categories_update = request.data.get('categories', '')
-        size_update = request.data.get('size', '')
-        count_update = request.data.get('count', '')
-        comments_update = request.data.get('comments', '')
-        unlisted_update = request.data.get('unlisted', False)
-        description_update = request.data.get('description ', '')
-        visibility_update = request.data.get('visibility', '')
-        if title_update is not None:
-            posts.title = title_update
-        if contentType_update is not None:
-            posts.contentType = contentType_update
-        if content_update is not None:
-            posts.content = content_update
-        if source_update is not None:
-            posts.source = source_update
-        if origin_update is not None:
-            posts.origin = origin_update
-        if categories_update is not None:
-            posts.categories = categories_update
-        if size_update is not None:
-            posts.size = size_update
-        if count_update is not None:
-            posts.count = count_update
-        if comments_update is not None:
-            posts.comments = comments_update
-        if unlisted_update is not None:
-            posts.unlisted = unlisted_update
-        if description_update is not None:
-            posts.description = description_update
-        if visibility_update is not None:
-            posts.visibility = visibility_update
-        posts.save()
-        return Response("Posts Update")
+    def get_author_posts(self, request,*args, **kwargs):
+        author_id = request.user
+        author_posts = Post.objects.filter(author=author_id)
+        inf_ret = PostSerializer(author_posts, many=True)
+        return Response(inf_ret.data)
 
-    # PUT
+    def get_valid_post(self, request,*args, **kwargs):
+        author_id = request.user.id
+        inf_ret = []
+        all_posts = Post.objects.all()
+        for post in all_posts:
+            if post.visibility == "1":
+                inf_ret.append(post)
+            elif post.visibility == "2":
+                if Friend.objects.filter(friends=author_id):
+                    inf_ret.append(post)
+            elif post.visibility == "3":
+                if self.check_private_spe(request,post,author_id):
+                    inf_ret.append(post)
+        return Response(inf_ret)
+
+    def check_private_spe(self,request,post,author_id):#TODO
+        return True
+
+    
+    '''
     def put_post(self,request,*args, **kwargs):
         author_id = request.user
         post_id = request.get('id', '')
+        posts = Post.objects.get(id=post_id)
         title = request.data.get('title', '')
         contentType = request.data.get('contentType', '')
         content =request.data.get('content', '')
-        source = request.data.get('source', '')
-        origin = request.data.get('origin', '')
+        
         categories = request.data.get('categories', '')
-        size = request.data.get('size', '')
-        count = request.data.get('count', '')
-        comments = request.data.get('comments', '')
-        unlisted = request.data.get('unlisted', False)
+    
+        #comments = request.data.get('comments', '')
         description = request.data.get('description ', '')
         visibility = request.data.get('visibility', '')
-        data1 = {'title': title, 'id': f"{author_id}/posts/{post_id}",
-                 'source': source, 'origin': origin, 'description': description,
-                 'contentType': contentType, 'content': content, 'author': author_id,
-                 'categories': categories, 'count': count, 'size': size,
-                 'comments': comments, 'visibility': visibility, 'unlisted': unlisted}
-        inf_ret = Follower.objects.filter(followers=author_id)
-        if inf_ret is not None:
-            all_follower = Follower.objects.get(followers=author_id)
-            for follower in all_follower:
-                inbox = Inbox.objects.get(author=follower)
-                inbox.items.append(data1)
-                inbox.save()
-        inf_store = PostSerializer(data=data1)
-        inf_store.save()
+        if title is not None:
+            posts.title = title
+        if contentType is not None:
+            posts.contentType = contentType
+        if content is not None:
+            posts.content = content
+        if categories is not None:
+            posts.categories = categories
+        if description_update is not None:
+            posts.description = description_update
+        if visibilitye is not None:
+            posts.visibility = visibility
+        posts.save()
+        
+        if visibility=="2":
+            inf_ret = Friend.objects.filter(friends=author_id)
+            if inf_ret is not None:
+                all_friend = Friend.objects.get(friends=author_id)
+                for friend in all_friend:
+                    inbox = Inbox.objects.get(author=friend)
+                    inbox.items.append(data1)
+                    inbox.save()
+        #TODO: select 3
+
         return Response(inf_store.data)
 
     # DELETE
-    def delete_post(self, request,*args, **kwargs):
-        author_id = request.user
-        post_id = request.get('id', '')
-        post_id = author_id + post_id
-        post = Post.objects.get(id=post_id)
-        post.delete()
-        return Response("Post Deleted")
+    '''
 
+
+class EditPostView(View):
+    def get(self, request, post_id):
+        cur_post = post_id  # TODO: find the post
+        context = {
+            'cur_post': cur_post
+        }
+        return render(request, 'edit_post.html', context=context)
+
+    def post(self, request, *args, **kwargs):
+        post_id = request.get('id', '')
+        cur_post = Post.objects.get(id=post_id)
+        title_update = request.POST.get('title', '')
+        contentType_update = request.POST.get('contentType', '')
+        content_update = request.POST.get('content', '')
+        categories_update = request.POST.get('categories', '')
+        categories_update = process_categories(categories_update)
+        description_update = request.POST.get('description ', '')
+        if title_update is not None:
+            cur_post.title = title_update
+        if contentType_update is not None:
+            cur_post.contentType = contentType_update
+        if content_update is not None:
+            cur_post.content = content_update
+        if categories_update is not None:
+            cur_post.categories = categories_update
+        if description_update is not None:
+            cur_post.description = description_update
+        cur_post.save()
+        return Response("Post Updated")
+
+
+def delete_post(request, id, post_id):
+    post_id = id + post_id
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return Response("Post Deleted")
+
+
+class SpecificPostView(View):
+    def get(self, request, author_id, post_id):
+        my_id = request.user.id
+        post = Post.objects.get(id=post_id)
+        liked = False #TODO
+        im_author = False
+        if str(my_id) == str(author_id):
+            im_author = True
+
+        context = {
+            'post': post,
+            'liked': liked,
+            'author__id': author_id,
+            'isAuthor': im_author
+
+        }
+        return render(request, 'post_legal.html', context=context)
