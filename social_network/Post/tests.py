9fa366834d2422d2973ae38b6b43a2db0c996229
zoujunqi <zou5@ualberta.ca>
2021-10-30 01:27:19 +0800

    Update tests.py

--- social_network/Post/tests.py
+++ social_network/Post/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.test.utils import setup_test_environment
+import uuid
+from django.contrib.auth.models import User
+from Author.models import Post, User
+from .models import PostLike, PostComment
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def setUp(self):
+        Post.objects.create(title='hello',
+            contentType='text/plain',
+            categories='hahaha',
+            description='hello world',
+            visibility=1,
+            select_user=1,
+            id='post_id_1',
+            image='C:/temp/test.png')
+
+        User.objects.create(
+            id='user_id_1',
+            username="username",
+            email="user@ualberta.ca",
+            first_name="user first",
+            last_name="user last",
+            is_active=True
+        )
+
+    def test_model_PostLike(self):
+        PostLike.objects.create(post=Post.objects.get(id='post_id_1'), who_like=User.objects.get(id='user_id_1'))
+
+        post_like = PostLike.objects.all()
+        AssertTrue(post_like., "")
+
