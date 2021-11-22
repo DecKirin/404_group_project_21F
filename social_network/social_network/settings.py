c768d2afeceab5be0d01cce1eddc092f1ea9b0ae
DecKirin <lxd199860@gmail.com>
2021-11-21 21:13:38 -0700

    Add heroku to setting

--- social_network/social_network/settings.py
+++ social_network/social_network/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import sys
+import django_on_heroku # top of the file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,5 @@ REST_FRAMEWORK = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
