117243884d833c6b4d726230652bc2e4d1fa62da
DecKirin <lxd199860@gmail.com>
2021-11-21 22:11:44 -0700

    Migrate

--- social_network/settings.py
+++ social_network/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import sys
-import django_on_heroku # top of the file
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
