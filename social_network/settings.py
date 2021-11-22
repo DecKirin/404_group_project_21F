5f2f321f5a77d10656d3456d0fef6478667ac23e
DecKirin <lxd199860@gmail.com>
2021-11-21 23:12:03 -0700

    static_root change

--- social_network/settings.py
+++ social_network/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.AllowAllUsersModelBackend']
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
