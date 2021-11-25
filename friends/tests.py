8e686b2ec4d1a95e770f4e118ce57232b28c5228
XZPshaw <zepeng@ualberta.ca>
2021-11-25 10:25:51 -0700

    fix some api issues caused by merging

--- friends/tests.py
+++ friends/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from .models import Friend, FriendRequest
 from Author.models import User
-
+from Author.serializers import UserSerializer
 
 class FriendTestCase(TestCase):
 
@@ -19,14 +19,14 @@ class FriendTestCase(TestCase):
     def test_can_add_friend(self):
         friend = Friend.objects.get(user=self.user1)
         self.assertEqual(len(friend.friends), 0)
-        friend.add_friend(self.user2)
+        friend.add_friend(UserSerializer(self.user2).data)
         self.assertEqual(len(friend.friends), 1)
 
     def test_can_delete_friend(self):
         friend = Friend.objects.get(user=self.user1)
-        friend.add_friend(self.user2)
+        friend.add_friend(UserSerializer(self.user2).data)
         self.assertEqual(len(friend.friends), 1)
-        friend.delete_friend(self.user2)
+        friend.delete_friend(UserSerializer(self.user2).data)
         self.assertEqual(len(friend.friends), 0)
 
 
@@ -44,23 +44,25 @@ class FriendRequestTestCase(TestCase):
     def test_can_accept_friend_request(self):
         friend1 = Friend.objects.create(user=self.user1)
         friend2 = Friend.objects.create(user=self.user2)
-        request = FriendRequest.objects.create(sender=self.user1, receiver=self.user2)
-        self.assertEqual(friend1.friends.all().count(), 0)
-        self.assertEqual(friend2.friends.all().count(), 0)
+        request = FriendRequest.objects.create(sender=UserSerializer(self.user1).data,
+                                               receiver=UserSerializer(self.user2).data)
+        self.assertEqual(len(friend1.friends), 0)
+        self.assertEqual(len(friend2.friends), 0)
         self.assertFalse(request.respond_status)
         request.accept_request()
-        self.assertEqual(friend1.friends.all().count(), 1)
-        self.assertEqual(friend2.friends.all().count(), 1)
+        self.assertEqual(len(friend1.friends), 1)
+        self.assertEqual(len(friend2.friends), 1)
         self.assertTrue(request.respond_status)
 
     def test_can_decline_friend_request(self):
         friend1 = Friend.objects.create(user=self.user1)
         friend2 = Friend.objects.create(user=self.user2)
-        request = FriendRequest.objects.create(sender=self.user1, receiver=self.user2)
-        self.assertEqual(friend1.friends.all().count(), 0)
-        self.assertEqual(friend2.friends.all().count(), 0)
+        request = FriendRequest.objects.create(sender=UserSerializer(self.user1).data,
+                                               receiver=UserSerializer(self.user2).data)
+        self.assertEqual(len(friend1.friends), 0)
+        self.assertEqual(len(friend2.friends), 0)
         self.assertFalse(request.respond_status)
         request.decline_request()
-        self.assertEqual(friend1.friends.all().count(), 0)
-        self.assertEqual(friend2.friends.all().count(), 0)
+        self.assertEqual(len(friend1.friends), 0)
+        self.assertEqual(len(friend2.friends), 0)
         self.assertTrue(request.respond_status)
