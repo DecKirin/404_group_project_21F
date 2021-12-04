089ea7688f497ffbbb576efa70cccefe7a6b36bf
DecKirin <lxd199860@gmail.com>
2021-12-04 09:47:08 -0700

    CORS enable

--- social_network/settings.py
+++ social_network/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Post.apps.PostConfig',
+    'corsheaders',
 
 ]
 
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'social_network.urls'
