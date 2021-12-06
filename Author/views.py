3bc522bff74fae673d0d82c5b15ee62c654dc689
XZPshaw <zepeng@ualberta.ca>
2021-12-06 15:20:14 -0700

    update some frontend

--- Author/views.py
+++ Author/views.py
@@ -97,7 +97,7 @@ def get_remote_nodes():
     # to test with team17
 
     #all_host = ["https://cmput404f21t17.herokuapp.com", "https://social-distribution-fall2021.herokuapp.com","https://cmput404-team13-socialapp.herokuapp.com"]
-    all_host = ["https://cmput404f21t17.herokuapp.com"]
+    #all_host = ["https://cmput404f21t17.herokuapp.com"]
     return all_host
 
 
