1e541fe2fc87a78fdc532d77e90a5909ea4bfe5c
Qianqiu Zhang <43324225+zqq66@users.noreply.github.com>
2021-12-06 14:33:03 +0800

    Update views.py

--- friends/views.py
+++ friends/views.py
@@ -242,7 +242,7 @@ class remote_sent_request(APIView):
         request = make_api_post_request(inbox_url, inbox_info)
         response = Response()
         response.status_code = 200
-        response.data = request
+        response.data = request.body
         return response
         return redirect(reverse('Author:my_list', kwargs={'relationship':'follows'}))
 
