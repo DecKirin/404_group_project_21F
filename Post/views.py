d1794e0908ee6fffd40dae73d21b200382e0dfea
zoujunqi <zou5@ualberta.ca>
2021-12-06 22:56:58 +0800

    update share

--- Post/views.py
+++ Post/views.py
@@ -382,7 +382,7 @@ def share_local_post(request, author_id, post_id):
     source_post = Post.objects.get(id=post_id)
     visibility = source_post.visibility
     if visibility != 1 and visibility != 2:
-        return
+        return redirect(reverse('Author:specific_post', args=(author_id, post_id)))
     title = source_post.title
     post_id = uuid.uuid4().hex
     post_id = str(post_id)
@@ -421,8 +421,104 @@ def share_local_post(request, author_id, post_id):
     return redirect(reverse('Author:index'))
 
 
-def share_remote_post(request, post_author_id, post_id):
-    ...
+def share_remote_post(request):
+    postAPIURL = request.GET.get("post_url")
+    postAPIURL = urllib.parse.unquote(postAPIURL)
+    postRequest = make_api_get_request(postAPIURL)
+    post = postRequest.json()
+    print(post)
+
+    team_flag = 0
+
+    try:
+        author_413 = post["author"]
+        host_413 = author_413["host"]
+        if host_413 == "http://cmput404-team13-socialapp.herokuapp.com":
+            team_flag = 13
+            source = post["url"]
+        elif host_413 == "https://social-distribution-fall2021.herokuapp.com/api/":
+            team_flag = 4
+            source = post["id"]
+    except Exception:  # TODO
+        print("Wrong")
+        '''
+        all_info = post["items"]
+        author_17 = all_info["author"]
+        host_17 = author_17["host"]
+        if host_17 == "https://cmput404f21t17.herokuapp.com/":
+            team_flag = 17 '''
+
+    if team_flag == 0:
+        print("The host is not in our connected group")
+        return redirect(reverse('Author:remote_specific_post') + "?post_url=%s" % postAPIURL)
+
+    #image = post['contentType'] + post['content']
+
+    isPublic = False
+    isFriend = False
+    try:
+        if str(post.visibility).lower() == "pb" or str(post.visibility).lower() == "public":
+            isPublic = True
+        if str(post.visibility).lower() == "fr" or str(post.visibility).lower() == "friends only":
+            # T13: FRIENDS
+            isFriend = True
+    except Exception:
+        if str(post["visibility"]).lower() == "pb" or str(post["visibility"]).lower() == "public":
+            isPublic = True
+        if str(post["visibility"]).lower() == "fr" or str(post["visibility"]).lower() == "friends only":
+            isFriend = True
+
+    if isPublic:
+        visibility = 1
+    elif isFriend:
+        visibility = 2
+    else:
+        # It will be a private post and we can't share it, do nothing
+        return redirect(reverse('Author:remote_specific_post') + "?post_url=%s" % postAPIURL)
+
+    title = post["title"]
+    post_id = uuid.uuid4().hex
+    post_id = str(post_id)
+
+    origin = post["origin"]
+    description = post["description"]
+    content_type = post["contentType"]
+    content = post["content"]
+    try:
+        categories = post["categories"]
+    except Exception:
+        categories = ''
+    try:
+        image = post["image"]
+    except Exception:
+        image = ''
+    post = Post.objects.create(visibility=visibility, title=title, id=post_id, source=source,
+                               origin=origin, description=description, contentType=content_type,
+                               content=content, author=request.user, categories=categories,
+                               image=image)
+    if post.author.url != "":
+        post.url = post.author.url + "posts/" + post.id + "/"
+        post.api_url = post.author.api_url + "posts/" + post.id + "/"
+    else:
+        post.url = request.scheme + "://" + request.META['HTTP_HOST'] + "/author/" + str(
+            post.author.id) + "/posts/" + post.id + "/"
+        post.api_url = request.scheme + "://" + request.META['HTTP_HOST'] + "/api/author/" + str(
+            post.author.id) + "/posts/" + post.id + "/"
+    if visibility == 2:
+        try:
+            friends = Friend.objects.get(user=author)
+            for friend in friends.friends:
+                print(friend)
+                fri_obj = User.objects.get(id=friend['uuid'])
+                inbox, status = Inbox.objects.get_or_create(author=fri_obj)
+                print(inbox.items)
+                inbox.items.append(PostSerializer(post).data)
+                inbox.save()
+        except:
+            pass
+    post.save()
+
+    return redirect(reverse('Author:index'))
 
 
 class like_remote_post_view(View):
